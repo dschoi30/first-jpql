e09073c3d3ceaaa460e117a3acb6f30f77e0597a
dschoi30 <ds0631@gmail.com>
2022-11-24 10:14:32 +0900

    ~jpql 1

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,50 @@
+package jpql;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member= new Member();
+            member.setUsername("관리자");
+            member.setAge(10);
+            member.setType(MemberType.ADMIN);
+
+            member.setTeam(team);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            String query = "select size(t.members) from Member m";
+            List<Integer> result = em.createQuery(query, Integer.class).getResultList();
+
+            for (Integer s : result) {
+                System.out.println("s = " + s);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+}
